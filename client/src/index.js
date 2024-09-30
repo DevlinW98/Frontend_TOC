37c7b8eb0f8a55e63853510d89303e3498ddf6e7
DevlinW98 <80415171+DevlinW98@users.noreply.github.com>
2024-10-01 00:14:49 +0700

    Add_Source_Code_Botton

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React,{} from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Show_Source_Code from './Show_Source_Code';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div><App/></div>,
+  },
+  {
+    path: "/SourceCode",
+    element: <div><Show_Source_Code/></div>,
+  },
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
